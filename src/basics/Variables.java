ee73b544c0d0b03912e8a1efa6751b3c93da9724
Hans Morales <hwnss.w@gmail.com>
2025-07-14 02:37:59 -0400

    Se agrega nuevas cosas al repo (cosas sin importancia solo es practica y experimentos).

--- src/basics/Variables.java
+++ src/basics/Variables.java
@@ -3,9 +3,24 @@ package basics;
 public class Variables {
     public static void main(String[] args) {
 
-        int num = 1;
-        double num2 = 5.5;
-        System.out.println(num + num2);
+        int numEntero = 15;
+
+        /* Se usa el sufijo de f para que tome validez,
+         sino lo toma asume un double.*/
+
+        //Precision simple
+        float numFlotante = 1.5f;
+
+        //Precision doble
+        double numDoble = 4.4d;
+
+        short numShort = Short.MAX_VALUE;
+
+        byte numByte = Byte.MAX_VALUE;
+        
+        String cadenaDeTexto = "Hola buenos dias hoy es %d de Julio".formatted(numEntero);
+        System.out.println("cadenaDeTexto = " + cadenaDeTexto);
+
 
     }
 }
